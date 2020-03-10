2230fe5c140e929e9f6c7d76a4d4c23e57de031b
Josh Armentano <josh@abidewebdesign.com>
2020-03-10 08:30:44 -0700

    Added GA.

--- header.php
+++ header.php
@@ -18,6 +18,16 @@ defined( 'ABSPATH' ) || exit;
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-160227746-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		
+		gtag('config', 'UA-160227746-1');
+	</script>
+
 </head>
 
 <body <?php body_class(); ?>>
